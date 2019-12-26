24c0f05e8292713003346deec7f8260581ecca98
NecroSith <necrosith@yandex.ru>
2019-12-26 21:53:07 +0300

    Init commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// Second argument is the list of dependencies
+const myApp = angular.module('myApp', []);
+
+// Controller defines a place to put the code associated with the module
+myApp.controller('mainController', function() {
+
+});
