1fb8c76ad3b49669ce4723e19dae4992160cc6d0
NecroSith <necrosith@yandex.ru>
2019-12-26 22:36:25 +0300

    Added info on . TODO: add info on dependency injections

--- app.js
+++ app.js
@@ -2,6 +2,16 @@
 const myApp = angular.module('myApp', []);
 
 // Controller defines a place to put the code associated with the module
-myApp.controller('mainController', function() {
+myApp.controller('mainController', function($scope) {
+
+
+    // Scope is a middlething between the view and controller
+    // we can add properties to it (from vars to functions, basically everything) and view will have access to them too
+    // but only inside this controller (mainController in this case)
+    $scope.name = 'Yan';
+    $scope.getName = function() {
+        console.log($scope.name);
+    }
+    console.log($scope);
 
 });
