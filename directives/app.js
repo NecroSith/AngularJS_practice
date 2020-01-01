58159e424e5f9676bdc8788a5ebdb6d34d0dad8b
Yan_Pustynnyy <necrosith@yandex.ru>
2020-01-01 20:56:00 +0300

    Added info on two-way data binding using ng-model

---
+++ directives/app.js
@@ -0,0 +1,10 @@
+const myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ["$scope", "$filter", function($scope, $filter) {
+
+    $scope.name = "Yan";
+    $scope.twitterId = '';
+    $scope.twitterIdLowerCase = function() {
+        return $filter('lowercase')($scope.twitterId);
+    }
+}]);
