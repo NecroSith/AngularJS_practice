0ba4387b6ec4cea798ae9247f2a2567a7b78f783
Yan_Pustynnyy <necrosith@yandex.ru>
2020-01-05 23:05:15 +0300

    Added info on  being a singleton

--- hash-and-routing/routing/app.js
+++ hash-and-routing/routing/app.js
@@ -27,14 +27,24 @@ myApp.controller('mainController', ["$scope", "$log", "$location", function($sco
     // Output current hash value
     $log.info($location.path());
 
+    $log.main = 'Main';
+    $log.log($log); // main property is there
+
 }]);
 
 // App often has multiple controllers and multiple views (always actually)
 // but it's better to store them in separate js files
 
 // $routeParams is a part of ngRoute service and gives access to the route parameters 
-myApp.controller('secondController', ["$scope", "$routeParams", function($scope, $routeParams) {
+myApp.controller('secondController', ["$scope", "$routeParams", "$log", function($scope, $routeParams, $log) {
 
     $scope.name = $routeParams.num || 'default';
+
+    $log.second = 'Second';
+    $log.log($log); // second property is there AS WELL AS MAIN PROPERTY
+    //* Because $log object is singleton
+    //! Singleton is one and the only copy of an object
+    // $log uses only copy of its object and shares it between all controllers so if some property is added to $log in one of the controllers 
+    // it automatically becomes available to others
 
 }]);
