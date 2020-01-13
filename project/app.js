553493b71cde6dea4083813390029ff45f8db97a
Yan_Pustynnyy <necrosith@yandex.ru>
2020-01-14 00:15:28 +0300

    Weather app: added routing, updated UI

--- project/app.js
+++ project/app.js
@@ -1,1 +1,25 @@
 const weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: './pages/main.html',
+            controller: 'mainController'
+        })
+        .when('/details', {
+            templateUrl: './pages/details.html',
+            controller: 'forecastController'
+        });
+
+    $locationProvider.html5Mode(false).hashPrefix('');
+})
+
+weatherApp.controller('mainController', ["$scope", function($scope) {
+    console.log('main!');
+
+}]);
+
+weatherApp.controller('forecastController', ["$scope", function($scope) {
+    console.log('forecast!');
+
+}])
