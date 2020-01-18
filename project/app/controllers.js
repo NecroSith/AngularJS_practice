1f8ee7046558c41a1ec4ebd4fbcd7319936e0eb2
Yan_Pustynnyy <necrosith@yandex.ru>
2020-01-18 17:56:29 +0300

    Weather app: added form to upgrade UX

--- project/app/controllers.js
+++ project/app/controllers.js
@@ -1,14 +1,22 @@
-weatherApp.controller('mainController', ["$scope", "nameService", function($scope, nameService) {
+weatherApp.controller('mainController', ["$scope", "nameService", "$location", function($scope, nameService, $location) {
     $scope.city = nameService.name;
 
     $scope.$watch('city', function() {
         nameService.name = $scope.city;
     });
 
+    $scope.submit = function() {
+        $location.path("/details"); // redirecting to the details page upon submit
+    }
+
 }]);
 
 weatherApp.controller('forecastController', ["$scope", "$resource", "nameService", "$routeParams", function($scope, $resource, nameService, $routeParams) {
-    $scope.city = nameService.name;
+    $scope.formatCity = function(input) {
+        return input.toLowerCase().replace(/(?:^|\s)\S/g, function(val) { return val.toUpperCase() })
+    };
+
+    $scope.city = $scope.formatCity(nameService.name);
 
     $scope.$watch('city', function() {
         nameService.name = $scope.city;
@@ -26,9 +34,9 @@ weatherApp.controller('forecastController', ["$scope", "$resource", "nameService
 
     $scope.convertToCelcius = function(degK) {
         return Math.round(degK - 273, 4);
-    }
+    };
 
     $scope.convertToDate = function(dt) {
         return new Date(dt * 1000);
-    }
+    };
 }]);
