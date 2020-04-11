d121fa2c24bca7d0ffea61776e53f38a846226cd
Yan_Pustynnyy <necrosith@yandex.ru>
2020-04-11 20:54:34 +0300

    Preparing to functionality extension

--- project/app/services.js
+++ project/app/services.js
@@ -4,16 +4,18 @@ weatherApp.service('nameService', function() {
 
 
 weatherApp.service('weatherService', ["$resource", "$routeParams", function($resource, $routeParams) {
+    this.apiCurrent = "http://api.weatherbit.io/v2.0/forecast/current?";
     this.api = "http://api.weatherbit.io/v2.0/forecast/daily?";
     this.appId = "e1dc1fb1392d41589351954694c7c655";
 
-    this.getWeather = function(city) {
-        weatherAPI = $resource(this.api, {
+    this.getWeather = function(city, current = false) {
+        const api = current ? this.apiCurrent : this.api;
+        weatherAPI = $resource(api, {
             get: {
                 method: "JSONP" // for browser to not block the request
             }
         });
 
         return weatherAPI.get({ city: city, key: this.appId, days: $routeParams.days || 2 });
-    }
+    };
 }])
